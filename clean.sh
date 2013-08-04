56c6c61f32fdc4c3c8c6c367445d7155f89b4505
M. H. J. Bramwell <mhb.warehouseman@gmail.com>
2013-08-04 08:45:42 -0400

    Clean now eliminates all xpi files and all editor temporary fall back files.  On branch master  Changes to be committed:
    	modified:   clean.sh

--- clean.sh
+++ clean.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-APP_NAME=SELBlocks  # short-name, jar and xpi files name. Must be lowercase with no spaces
-echo "Removing temporary files and $APP_NAME.xpi..."
+echo "Removing temporary files and the 'xpi' file..."
 echo "You can run build.sh to recreate them."
 
-rm -f $APP_NAME.xpi
+find . -name "*~" -print0 | xargs -0 rm -rf
+
+rm -f *.xpi
 rm -fr build
 
